5ac0564c4f057b83fdfe3d3daacbc8224ea704fb
unknown <paulas@Paulas-PC.lar2.mobilestreams.com>
2015-10-20 16:45:30 -0300

    nada

---
+++ app/routes.js
@@ -0,0 +1,34 @@
+
+
+var contactController = require('../app/controllers/contact');
+// expose the routes to our app with module.exports
+module.exports = function(app, router) {
+    
+    router.route('/contacts')
+      .post(contactController.postContacts)
+      .get(contactController.getContacts);
+
+    router.route('/contacts/:contact_id')
+       .get(contactController.getContact)
+       .delete(contactController.deleteContact);
+
+
+    // Register all our routes with /api
+    app.use('/api', router);
+
+
+    var path = require('path');
+
+    // application -------------------------------------------------------------
+    app.get('/', function(req, res) {
+        res.sendFile(path.resolve('./public/index.html'));
+        //res.sendFile(__dirname + '/public/index.html');
+    });
+
+    //var webRoutes=require(path.resolve(__dirname + '/public'));
+    app.get('*',function(req,res){
+
+        var dir= __dirname.toString().replace('app','');
+        res.sendFile(dir + '/public/' + req.url);
+    });
+};
